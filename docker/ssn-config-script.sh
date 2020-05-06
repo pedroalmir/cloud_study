e941ec8c181ed912af223210902b291c2b416774
Pedro Almir <petrus.cc@gmail.com>
2020-05-06 18:49:49 -0300

    ssn-config-script 0.2

--- docker/ssn-config-script.sh
+++ docker/ssn-config-script.sh
@@ -1,17 +1,16 @@
 # https://labs.play-with-docker.com/
 # wget https://raw.githubusercontent.com/pedroalmir/cloud_study/master/docker/ssn-config-script.sh
 # chmod +x ssn-config-script.sh
-# ssn-config-script.sh
+# \. ssn-config-script.sh
 echo "SSN-Config Script"
 echo "Creating folder structure..." 
 sudo rm -rf ssn-docker
 mkdir -p ssn-docker/volumes/dababase
 mkdir -p ssn-docker/volumes/backend
-mkdir -p ssn-docker/volumes/frontend
 echo "Downloading the docker-compose file..."
 wget https://raw.githubusercontent.com/pedroalmir/cloud_study/master/docker/docker-compose.yml -O ssn-docker/docker-compose.yml
 echo "Downloading the backend war file..."
 wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-8d_AHJL2cOSY001YqQ6Z43lE3eXTCep' -O ssn-docker/volumes/backend/ssnetwork-docker.war
 echo "Downloading and unzipping the frontend project..."
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1yUy4sINTdj9UqYDiCu94WExU1nDB842C' -O ssn-docker/volumes/frontend/frontend.zip
-unzip ssn-docker/volumes/frontend/frontend.zip
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1yUy4sINTdj9UqYDiCu94WExU1nDB842C' -O ssn-docker/volumes/frontend.zip
+unzip ssn-docker/volumes/frontend.zip -d ssn-docker/volumes/
