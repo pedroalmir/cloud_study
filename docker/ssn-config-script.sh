5742a6f6e3b546a00e9fb70f7c8c2fec9e6ced47
Pedro Almir <petrus.cc@gmail.com>
2020-05-07 00:56:32 -0300

    ssn-config-script 0.5

--- docker/ssn-config-script.sh
+++ docker/ssn-config-script.sh
@@ -1,17 +1,42 @@
 # https://labs.play-with-docker.com/
 # wget https://raw.githubusercontent.com/pedroalmir/cloud_study/master/docker/ssn-config-script.sh
-# chmod +x ssn-config-script.sh
 # \. ssn-config-script.sh
+# Dir stucture:
+# |_ ssn-docker
+#    |_ dockerfile-backend
+#    |_ dockerfile-frontend
+#    |_ docker-compose.yml
+#    |_ dababase
+#    |_ backend
+#       |_ ssnetwork-docker.war
+#    |_ frontend
+#       |_ [frontend dirs and files]
+
 echo "SSN-Config Script"
 echo "Creating folder structure..." 
-sudo rm -rf ssn-docker > /dev/null 2>&1
-mkdir -p ssn-docker/volumes/dababase > /dev/null 2>&1
-mkdir -p ssn-docker/volumes/backend > /dev/null 2>&1
-echo "Downloading the docker-compose file..."
-wget https://raw.githubusercontent.com/pedroalmir/cloud_study/master/docker/docker-compose.yml -O ssn-docker/docker-compose.yml > /dev/null 2>&1
+sudo rm -rf ssn-docker
+mkdir -p ssn-docker/dababase
+mkdir -p ssn-docker/backend
+
+echo "Downloading the docker files..."
+wget -nv https://raw.githubusercontent.com/pedroalmir/cloud_study/master/docker/docker-compose.yml -O ssn-docker/docker-compose.yml
+wget -nv https://raw.githubusercontent.com/pedroalmir/cloud_study/master/docker/dockerfile-backend -O ssn-docker/dockerfile-backend
+wget -nv https://raw.githubusercontent.com/pedroalmir/cloud_study/master/docker/dockerfile-frontend -O ssn-docker/dockerfile-frontend
+
 echo "Downloading the backend war file..."
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-8d_AHJL2cOSY001YqQ6Z43lE3eXTCep' -O ssn-docker/volumes/backend/ssnetwork-docker.war > /dev/null 2>&1
+wget -nv --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-8d_AHJL2cOSY001YqQ6Z43lE3eXTCep' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1-8d_AHJL2cOSY001YqQ6Z43lE3eXTCep" -O ssnetwork-docker.war && rm -rf /tmp/cookies.txt
+
 echo "Downloading and unzipping the frontend project..."
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1yUy4sINTdj9UqYDiCu94WExU1nDB842C' -O ssn-docker/volumes/frontend.zip > /dev/null 2>&1
-unzip -qq ssn-docker/volumes/frontend.zip -d ssn-docker/volumes/
+wget -nv --no-check-certificate 'https://docs.google.com/uc?export=download&id=1yUy4sINTdj9UqYDiCu94WExU1nDB842C' -O ssn-docker/frontend.zip
+unzip -qq ssn-docker/frontend.zip -d ssn-docker/
+rm ssn-docker/frontend.zip
+
+echo "Building docker images..."
+cd ssn-docker/
+docker build -f dockerfile-backend -t pedroalmir/tomcat:0.1 .
+docker build -f dockerfile-frontend -t pedroalmir/python:0.1 .
+
+# docker run --rm -d --name backend -p 80:8080 pedroalmir/mytomcat:0.1
 echo "Done!"
+
+
